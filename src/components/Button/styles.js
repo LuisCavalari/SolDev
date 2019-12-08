2c4a1fe9759f082288fe73387202517ad44c4941
Luis Cavalari <lfelipe.felipe4@gmail.com>
2019-12-08 18:14:31 -0300

    Button component add

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  background-color:#6e0ad6;
+  color:#fff;
+  font-weight:bold;
+  border:none;
+  border-radius:5px;
+  padding:10px;
+  cursor:pointer;
+  outline:none;
+  &:hover{
+    opacity:0.8;
+  }
+`;
